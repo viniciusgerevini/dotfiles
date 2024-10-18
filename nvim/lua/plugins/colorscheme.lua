c9c568f9760a780c09be04ecfa7414f9117c9b2d
Vinicius Gerevini <vgerevini@atlassian.com>
2024-10-18 16:19:42 -0400

    add extra colorschemes

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,11 @@
 return {
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
   'EdenEast/nightfox.nvim', -- colorscheme
+
+  dependencies = {
+    'rmehri01/onenord.nvim',
+    'catppuccin/nvim',
+  },
   priority = 1000, -- Make sure to load this before all the other start plugins.
   init = function()
     -- Load the colorscheme here.
